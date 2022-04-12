3239a22905f7bef2d8327c35e39763f315be291b
Zimmius <grybakov@usetech.ru>
2022-04-12 22:13:02 +0300

    Добавьте файлы проекта.

---
+++ mantis-tests/tests/AccountCreationTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using TestContext = NUnit.Framework.TestContext;
+
+namespace mantis_tests
+{
+    /// <summary>
+    /// Сводное описание для AccountCreationTests
+    /// </summary>
+    [TestFixture]
+    public class AccountCreationTests : TestBase
+    {
+        [OneTimeSetUp]
+        public void setUpConfig()
+        {
+            app.Ftp.BackupFile("config/config_inc.php");
+            using (Stream localFaile = File.Open(TestContext.CurrentContext.TestDirectory + "/config_inc.php", FileMode.Open))
+            {
+                app.Ftp.Upload("config/config_inc.php", localFaile);
+            }
+        }
+
+        [Test]
+        public void TestAccountRegistration()
+        {
+            AccountData account = new AccountData()
+            {
+                Name = "testuser",
+                Password = "password",
+                Email = "testuser@localhost.localdomain"
+            };
+
+            app.Registration.Register(account);
+        }
+
+        [OneTimeTearDown]
+        public void restoreConfig()
+        {
+            app.Ftp.RestoreBackupFile("config/config_inc.php");
+        }
+    }
+}
